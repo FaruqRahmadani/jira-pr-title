42ee81910038727497cb2ffb5fc35fa2c2e7b413
faruqrahmadani <faruq.rahmadani@gmail.com>
2022-07-28 10:39:20 +0800

    init push

---
+++ index.js
@@ -0,0 +1,86 @@
+import * as core from "@actions/core";
+import * as github from "@actions/github";
+
+async function run(){
+  const githubToken = core.getInput('GITHUB_TOKEN');
+  const octokit = github.getOctokit(githubToken)
+  
+  checkTitle().then(result => {
+    if(result){
+      removeLabel(octokit);
+    }else{
+      addLabel(octokit);
+    }
+  })
+}
+
+async function checkTitle(){
+  const title = github.context.payload.pull_request.title;
+  
+  const allowedJiraTickets = core.getInput('ALLOWED_JIRA_TICKET');
+  const allowedJiraTicketArray = allowedJiraTickets.split(',');
+  let regexpPRTitle = new RegExp(".*\\b("+allowedJiraTicketArray.join('|')+")\\b.\\d{0,}[0-9]", 'i');
+
+  if (regexpPRTitle.test(title)) {
+    return true;
+  }
+  return false;
+}
+
+async function addLabel(octokit){
+  initLabel();
+  const label_name = core.getInput('LABEL_TITLE_FAILED');
+
+  const [owner, repo] = process.env.GITHUB_REPOSITORY.split("/");
+  const issue_number = github.context.issue.number;
+
+
+  core.info(`Adding label (${label_name}) to PR...`);
+  
+  addLabelResponse = await octokit.issues.addLabels({
+    owner,
+    repo,
+    issue_number,
+    labels: [label_name],
+  });
+
+  core.info(`Added label (${label_name}) to PR - ${addLabelResponse.status}`);
+}
+
+async function removeLabel(octokit){
+  const label_name = core.getInput('LABEL_TITLE_FAILED');
+
+  const [owner, repo] = process.env.GITHUB_REPOSITORY.split("/");
+  const issue_number = github.context.issue.number;
+
+  core.info(`Removing label (${label_name}) to PR...`);
+  
+  await octokit.issues.addLabels({
+    owner,
+    repo,
+    issue_number,
+    labels: [label_name],
+  });
+
+  core.info(`Removed label (${label_name}) to PR - ${addLabelResponse.status}`);
+}
+
+async function initLabel(octokit){
+  const label_name = core.getInput('LABEL_TITLE_FAILED');
+  const label_color = core.getInput('LABEL_COLOR_FAILED');
+
+  const [owner, repo] = process.env.GITHUB_REPOSITORY.split("/");
+
+  core.info(`Initialize label (${label_name})...`);
+  
+  addLabelResponse = await octokit.issues.createLabel({
+    owner,
+    repo,
+    label_name,
+    color: label_color,
+  });
+
+  core.info(`Finish initialize label (${label_name})`);
+}
+
+run();
